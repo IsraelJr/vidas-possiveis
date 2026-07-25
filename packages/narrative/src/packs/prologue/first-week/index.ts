e6e8e25fab932ffda595a5191d84ae23cfe50474
Israel <32980364+IsraelJr@users.noreply.github.com>
2026-07-25 16:16:08 -0300

    feat: register Tuesday closure scenes

--- packages/narrative/src/packs/prologue/first-week/index.ts
+++ packages/narrative/src/packs/prologue/first-week/index.ts
@@ -4,11 +4,13 @@ import { firstWeekPart2 } from "./part-2";
 import { firstWeekPart3 } from "./part-3";
 import { firstWeekPart4 } from "./part-4";
 import { firstWeekPart5 } from "./part-5";
+import { tuesdayClosureNodes } from "./tuesday-closure";
 
 export const firstWeekNodes = [
   ...firstWeekPart1,
   ...firstWeekPart2,
   ...firstWeekPart3,
+  ...tuesdayClosureNodes,
   ...firstWeekPart4,
   ...dayClosureNodes,
   ...firstWeekPart5
