525901abfbb686b71d98f94ad0e16ba830ce6f9f
Israel <32980364+IsraelJr@users.noreply.github.com>
2026-07-26 03:53:49 -0300

    fix: remove hidden next-day clock jump from gossip choices

--- packages/narrative/src/packs/prologue/first-week/index.ts
+++ packages/narrative/src/packs/prologue/first-week/index.ts
@@ -6,12 +6,15 @@ import { firstWeekPart3 } from "./part-3";
 import { firstWeekPart4 } from "./part-4";
 import { firstWeekPart5 } from "./part-5";
 import { tuesdayClosureNodes } from "./tuesday-closure";
+import { addWednesdayTemporalContinuity } from "../wednesday-temporal-continuity";
+
+const temporallySafePart3 = addWednesdayTemporalContinuity(firstWeekPart3);
 
 export const firstWeekNodes = [
   ...firstWeekPart1,
   ...firstWeekPart2,
   ...mondayNightClosureNodes,
-  ...firstWeekPart3,
+  ...temporallySafePart3,
   ...tuesdayClosureNodes,
   ...firstWeekPart4,
   ...dayClosureNodes,
